308ada076de8d0abe7210f4cf363a7f30a8a4791
Andrew Vong <vongandrew5@gmail.com>
2021-11-03 14:20:49 -0700

    Created Game Loop, inserted player input & window clear/display into loop

--- Timber.cpp
+++ Timber.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 
 // Make code easier to type with "using namespace"
+// Prevents having to type "sf::VideoMode" and "sf::RenderWindow"
 using namespace sf;
 
 // This is where our game starts from
@@ -11,7 +12,38 @@ int main()
     VideoMode vm(1920, 1080);
 
     // Create and open a window for the game
+    // "Style::Fullscreen" is a constant defined by SFML
     RenderWindow window(vm, "Timber", Style::Fullscreen);
+
+    // Main Game Loop
+    while (window.isOpen())
+    {
+        /********************************
+            Handle the player's input  
+        ********************************/
+        
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+
+        /************************
+           Update the Scene    
+        *************************/
+
+
+        /********************
+            Draw the scene  
+        *********************/
+
+        //Clear everything from the last frame
+        window.clear();
+
+        //Draw our game scene here
+
+        //Show everything we just drew
+        window.display();
+    }
 
     return 0;
 }
